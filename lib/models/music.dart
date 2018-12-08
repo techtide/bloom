ffabcb723dfac541e01c59b9f614596feb43e18f
Arman B <techtide@users.noreply.github.com>
2018-12-08 17:21:06 +0000

    Added Robert's list of music

--- lib/models/music.dart
+++ lib/models/music.dart
@@ -1,4 +1,4 @@
-Track currentSelectedTrack = new Track(Tracks().getTracks()[0]["url"], int.parse(Tracks().getTracks()[0]["ageYear"]), Tracks().getTracks()[0]["title"], Tracks().getTracks()[0]["author"]);
+Track currentSelectedTrack = new Track(Tracks().getTracks()[0][ url ], int.parse(Tracks().getTracks()[0][ ageYear ]), Tracks().getTracks()[0][ title ], Tracks().getTracks()[0][ author ]);
 
 class Track {
   String url;
@@ -18,47 +18,49 @@ class Track {
 class Tracks {
   final List<Map<String, dynamic>> tracks = [
     // 1920
-    {"title": "Ain't Misbehavin", "author": "Fats Weller", "url": 'https://api.soundcloud.com/tracks/96188366/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P', "ageYear": '1920'},
-    {"title": "", "url": '', "author": "","ageYear": '1920'},
-    {"title": "", "url": '', "author": "","ageYear": '1920'},
-    {"title": "", "url": '', "author": "","ageYear": '1920'},
-    {"title": "", "url": '', "author": "","ageYear": '1920'},
+    {"title": "Ain t Misbehavin",  "url":  "https://api.soundcloud.com/tracks/96188366/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P", "author":  "Fats Weller",  "ageYear":  1920 },
+    {"title": "Dark was the night", "url":  "https://api.soundcloud.com/tracks/96188366/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P", "author":  "Blind Willie Johnson", "ageYear":  1920 },
+    {"title": "Down Hearted Blues", "url":  "https://api.soundcloud.com/tracks/135758251/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P", "author":  "Bessie Smith", "ageYear":  1920 },
+    {"title": "In the Jailhouse Now", "url":  "https://api.soundcloud.com/tracks/238012819/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P", "author":  "Jimmie Rodgers",  "ageYear":  1920 },
+    {"title": "Makin’ Whoopee!", "url":  "https://api.soundcloud.com/tracks/301296241/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P", "author":  "Bing Crosby", "ageYear": 1920},
+
+
     // 1930
-    {"title": "", "url": '', "author": "","ageYear": '1930'},
-    {"title": "", "url": '', "author": "","ageYear": '1930'},
-    {"title": "", "url": '',"author": "", "ageYear": '1930'},
-    {"title": "", "url": '',"author": "", "ageYear": '1930'},
-    {"title": "", "url": '', "author": "","ageYear": '1930'},
+    {"title": "In the Mood", "url": "https://api.soundcloud.com/tracks/1648392/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P", "author": "Glenn Miller",  "ageYear":  1930 },
+    {"title": "Over The Rainbow", "url": "https://api.soundcloud.com/tracks/58566455/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P",  "author":  "Judy Garland" , "ageYear":  1930 },
+    {"title": "God Bless America", "url": "https://api.soundcloud.com/tracks/254198178/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P",  "author" :  "Kate Smith", "ageYear":  1930 },
+    {"title": "Sing Sing Sing (With a Swing)","url":  "https://api.soundcloud.com/tracks/39189881/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P",  "author":  "Benny Goodman", "ageYear":  1930 },
+    {"title": "Silent Night, Holy Night", "url": "https://api.soundcloud.com/tracks/393732264/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P",  "author": "Bing Crosby", "ageYear":  1930 },
+
+
     // 1940
-    {"title": "", "url": '', "author": "","ageYear": '1940'},
-    {"title": "", "url": '',"author": "", "ageYear": '1940'},
-    {"title": "", "url": '', "author": "","ageYear": '1940'},
-    {"title": "", "url": '',"author": "", "ageYear": '1940'},
-    {"title": "", "url": '', "author": "","ageYear": '1940'},
-    // 1950
-    {"title": "", "url": '',"author": "", "ageYear": '1950'},
-    {"title": "", "url": '',"author": "", "ageYear": '1950'},
-    {"title": "", "url": '',"author": "", "ageYear": '1950'},
-    {"title": "", "url": '',"author": "", "ageYear": '1950'},
-    {"title": "", "url": '', "author": "","ageYear": '1950'},
+    {"title":  "White Christmas", "url": "https://api.soundcloud.com/tracks/30815780/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P", "author": "Bing Crosby" ,  "ageYear":  1940 },
+    {"title":  "The Christmas Song", "url":  "https://api.soundcloud.com/tracks/181371268/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P" , "author": "Nat  King  Cole", "ageYear":  1940 },
+    {"title":  "God Bless the Child", "url":  "https://api.soundcloud.com/tracks/53709132/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P", "author": "Billie Holiday",  "ageYear":  1940 },
+    {"title":  "Take the A Train",  "url":  "https://api.soundcloud.com/tracks/167314054/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P",  "author":  "Duke Ellington",  "ageYear":  1940 },
+    {"title":  "Star Dust",  "url": "https://api.soundcloud.com/tracks/297259559/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P", "author":  "Artie Shaw" ,  "ageYear" :  1940 },
+
+
+// 1950
+    {"title": "That’s Amore ,  url :  https://api.soundcloud.com/tracks/12112157/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P",  "author": "Dean Martin" ,  "ageYear":  1950 },
+    {"title": "Come Fly With Me ,  url :  https://api.soundcloud.com/tracks/141257734/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P", "author":  "Frank Sinatra", "ageYear":  1950 },
+    {"title": "Jailhouse Rock ,  url :  https://api.soundcloud.com/tracks/41575330/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P", "author":  "Elvis Presley", "ageYear":  1950 },
+    {"title": "I Walk The Line ,  url :  https://api.soundcloud.com/tracks/68892719/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P",  "author":  "Johnny Cash", "ageYear":  1950 },
+    {"title": "I ve Got You Under My Skin ,  url :  https://api.soundcloud.com/tracks/111613252/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P",  "author":  "Frank Sinatra", "ageYear":  1950 },
+    
     // 1960
-    {"title": "", "url": '',"author": "", "ageYear": '1960'},
-    {"title": "", "url": '',"author": "", "ageYear": '1960'},
-    {"title": "", "url": '',"author": "", "ageYear": '1960'},
-    {"title": "", "url": '',"author": "", "ageYear": '1960'},
-    {"title": "", "url": '',"author": "", "ageYear": '1960'},
+    {"title": "Sweet Caroline", "url":  "https://api.soundcloud.com/tracks/253089074/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P", "author": "Neil Diamond", "ageYear":  1960 },
+    {"title": "Shout",  "url":  "https://api.soundcloud.com/tracks/527214093/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P", "author":  "Isley Brothers", "ageYear":  1960 },
+    {"title": "Brown Eyed Girl",  "url":  "https://api.soundcloud.com/tracks/11814201/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P", "author":  "Van Morrison",  "ageYear":  1960 },
+    {"title": "The Way You Look Tonight",  "url":  "https://api.soundcloud.com/tracks/28345912/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P",  "author":  "Frank Sinatra",  "ageYear":  1960 },
+    {"title": "Twist And Shout",  "url":  "https://api.soundcloud.com/tracks/46619076/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P",  "author":  "The Beatles",  "ageYear":  1960 },
+    
     // 1970
-    {"title": "", "url": '',"author": "", "ageYear": '1970'},
-    {"title": "", "url": '',"author": "", "ageYear": '1970'},
-    {"title": "", "url": '',"author": "", "ageYear": '1970'},
-    {"title": "", "url": '',"author": "", "ageYear": '1970'},
-    {"title": "", "url": '',"author": "", "ageYear": '1970'},
-    // 1980
-    {"title": "", "url": '',"author": "", "ageYear": '1980'},
-    {"title": "", "url": '',"author": "", "ageYear": '1980'},
-    {"title": "", "url": '',"author": "", "ageYear": '1980'},
-    {"title": "", "url": '',"author": "", "ageYear": '1980'},
-    {"title": "", "url": '',"author": "", "ageYear": '1980'},
+    {"title":  "No Woman, No cry", "url" :  "https://api.soundcloud.com/tracks/84631702/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P", "author" :  "Bob Marley",  "ageYear" :  1970 },
+    {"title":  "Big Yellow Taxi",  "url":  "https://api.soundcloud.com/tracks/204560824/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P",  "author" :  "Joni Mitchell",  "ageYear":  1970 },
+    {"title":  "Bohemian Rhapsody", "url" :  "https://api.soundcloud.com/tracks/115417954/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P", "author" :  "Queen", "ageYear":  1970 },
+    {"title":  "Stayin’ Alive",  "url" :  "https://api.soundcloud.com/tracks/67283900/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P",  "author" :  "Bee Gees" , "ageYear":  1970 },
+    {"title":  "Dancing Queen",  "url" :  "https://api.soundcloud.com/tracks/187768173/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P",  "author" :  "ABBA" ,  "ageYear":  1970 },
   ];
   
   List<Map<String, dynamic>> getTracks() {
--- lib/pages/suggest.dart
+++ lib/pages/suggest.dart
@@ -11,25 +11,42 @@ class SuggestScreen extends StatefulWidget {
 
 class SuggestScreenState extends State<SuggestScreen> {
   Tracks tracks = new Tracks();
+  
+  TextEditingController searchController = new TextEditingController();
+  String filter;
 
   @override
   Widget build(BuildContext context) {
-    return new Center(
-      child: ListView.builder(
-        itemCount: tracks.getTracks().length,
-        itemBuilder: (BuildContext c, int index) {
-            if(tracks.getTracks()[index]["title"] != "") {
-              return new ListTile(
-                title: new Text(tracks.getTracks()[index]["title"]),
-                leading: new Icon(Icons.playlist_add),
-                onTap: () {
-                currentSelectedTrack = Track(tracks.getTracks()[index]["url"].toString(), int.parse(tracks.getTracks()[index]["ageYear"]), tracks.getTracks()[index]["title"].toString(), tracks.getTracks()[index]["author"].toString());
-              },
-            );
-          }
-        },
-        padding: EdgeInsets.only(left: 10, right:10, top:10, bottom:10),
-      ),
+    return new Column(
+      children: <Widget>[
+        new Padding(
+          padding: new EdgeInsets.only(top: 20.0),
+        ),
+        new TextField(
+          decoration: new InputDecoration(
+            labelText: "Patient's age",
+            border: InputBorder.none,
+          ),
+          controller: searchController,
+        ),
+        new Expanded(
+          child: new ListView.builder(
+            itemCount: tracks.getTracks().length,
+            itemBuilder: (BuildContext c, int index) {
+                if(tracks.getTracks()[index]["title"] != "") {
+                  return new ListTile(
+                    title: new Text(tracks.getTracks()[index]["title"]),
+                    leading: new Icon(Icons.playlist_add),
+                    onTap: () {
+                    currentSelectedTrack = Track(tracks.getTracks()[index]["url"].toString(), int.parse(tracks.getTracks()[index]["ageYear"]), tracks.getTracks()[index]["title"].toString(), tracks.getTracks()[index]["author"].toString());
+                  },
+                );
+              }
+            },
+            padding: EdgeInsets.only(left: 10, right:10, top:10, bottom:10),
+          ),
+        )
+      ]
     );
   }
 }
