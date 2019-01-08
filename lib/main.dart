2bb162f5c3a1edf98c51b3f317d2ab26b6e663b0
Arman B <techtide@users.noreply.github.com>
2019-01-08 10:42:54 +0000

    done

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
+        debugShowCheckedModeBanner: false,
         theme: new ThemeData(
           primaryColor: Colors.pink,
           accentColor: Colors.pinkAccent,
