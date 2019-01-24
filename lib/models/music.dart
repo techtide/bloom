a96756d2bbbb97660c6aea3029b6304679160917
Arman B <techtide@users.noreply.github.com>
2019-01-24 23:05:38 +0000

    somewhat better

--- lib/models/music.dart
+++ lib/models/music.dart
@@ -22,7 +22,7 @@ class Track {
 
 // Need 5 tracks for 1920, 1930, 1940, 1950, 1960, 1970, 1980
 class Tracks {
-  static List<dynamic> tracks_US_UK = [
+  static List<Map<String,dynamic>> tracks_US_UK = [
     // 1920
     {"title": "Ain't Misbehavin",  "url":  "https://api.soundcloud.com/tracks/96188366/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P", "author":  "Fats Weller",  "ageYear":  1920, "image": "https://images-na.ssl-images-amazon.com/images/I/81wW2UTy7xL._SL1500_.jpg"},
     {"title": "Dark was the night", "url":  "https://api.soundcloud.com/tracks/96188366/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P", "author":  "Blind Willie Johnson", "ageYear":  1920, "image": "http://nodepression.com/sites/default/files/styles/xlarge/public/images/main_image/ning_blog/BlindWillieJohnsonpic.jpg?itok=dQ1rXos1" },
@@ -69,7 +69,7 @@ class Tracks {
     {"title":  "Dancing Queen",  "url" :  "https://api.soundcloud.com/tracks/187768173/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P",  "author" :  "ABBA" ,  "ageYear":  1970, "image": "https://upload.wikimedia.org/wikipedia/en/thumb/e/ef/ABBA_-_Dancing_Queen.png/220px-ABBA_-_Dancing_Queen.png"},
   ];
 
-  static List<dynamic> tracks_INDIA = [
+  static List<Map<String,dynamic>> tracks_INDIA = [
     {"title": "Koi Hum Dum Na Raha", "url": "https://api.soundcloud.com/tracks/178170153/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P", "author": "Abhijeet Bhattacharyai", "ageYear":"1940", "image": "https://i.ytimg.com/vi/gGmy88f5QSo/hqdefault.jpg"},
 
     {"title": "Hum Aankh Macholi Kheleinge", "url": "https://api.soundcloud.com/tracks/295398377/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P", "author": "Khandan", "ageYear":"1940", "image": "https://i.ytimg.com/vi/-c9ihpOJp_Y/hqdefault.jpg"},
@@ -136,7 +136,7 @@ class Tracks {
     {"title": "Mizuiro no Ame", "url": "https://api.soundcloud.com/tracks/444847959/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P", "author": "Junko Yagami", "ageYear":"1980", "image": "https://media.ntslive.co.uk/crop/770x770/d7deabf7-4e65-42e3-93bf-0faa026f98b6_1522022400.png"},
   ];
 
-  static List<dynamic> tracks_CHINA = [
+  static List<Map<String,dynamic>> tracks_CHINA = [
     {"title": "The Moon Represents My Heart", "url": "https://api.soundcloud.com/tracks/146796808/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P", "author": "Teresa Teng", "ageYear":"1970", "image": "https://i1.sndcdn.com/artworks-000111252552-yxjb9s-t500x500.jpg"},
 
     {"title": "The Wandering Songstress", "url": "https://api.soundcloud.com/tracks/95032461/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P", "author": "Zhou Xuan", "ageYear":"1940", "image": "https://direct.rhapsody.com/imageserver/images/Alb.80832664/500x500.jpg"},
@@ -170,14 +170,15 @@ class Tracks {
       // tracks_CHINA = chinaList;
       // tracks_INDIA = indiaList;
       // tracks_JAPAN = japanList;
-      print(usUKList[0]["title"]);
+      // print(usUKList[0]["title"]);
       List<Map<String, dynamic>> newTrackList = new List<Map<String, dynamic>>();
-      // usUKList.forEach((element) => newTrackList.add({"title": element["title"], "url": element["url"], "author": element["author"], "year": element["year"], "image": element["image"]}));
-      // tracks_US_UK = usUKList;
+      usUKList.forEach((element) => newTrackList.add({"title": element["title"], "url": element["url"], "author": element["author"], "year": element["ageYear"], "image": (element["image"] != "none" || element["image"] != null ) ? element["image"] : "http://i.imgur.com/nszu54A.jpg"}));
+      tracks_US_UK = newTrackList as List<Map<String, dynamic>>;
+      print(newTrackList[0]["url"]);
     }
   }
 
-  static Future<bool> isVersionChanged() async {
+  static Future<bool> isVersionChanged() async {  
     final response = await http.get(versionServerURL);
     if(response.statusCode == 200) {
       if(response.body != config.version) {
